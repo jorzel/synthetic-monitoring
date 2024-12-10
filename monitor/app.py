38d21d858145ba33881003a03c9ac4811ded56c4
Jaroslaw Orzel <jaroslaw.orzel@saucelabs.com>
2024-12-10 14:16:08 +0100

    Set up synthetic monitor

---
+++ monitor/app.py
@@ -0,0 +1,72 @@
+import os
+import time
+import threading
+
+import prometheus_client
+from flask import Flask
+
+import structlog
+import requests
+from prometheus_client import Histogram, make_wsgi_app
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+
+logger = structlog.get_logger()
+
+app = Flask(__name__)
+app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {"/metrics": make_wsgi_app()})
+
+prometheus_client.REGISTRY.unregister(prometheus_client.GC_COLLECTOR)
+prometheus_client.REGISTRY.unregister(prometheus_client.PLATFORM_COLLECTOR)
+prometheus_client.REGISTRY.unregister(prometheus_client.PROCESS_COLLECTOR)
+
+INTERVAL_SEC = int(os.getenv("INTERVAL_SEC", 5))
+RESERVATIONS_SERVICE_URL = os.getenv("RESERVATIONS_SERVICE_URL")
+if not RESERVATIONS_SERVICE_URL:
+    raise ValueError("RESERVATIONS_SERVICE_URL is not set")
+
+HTTP_REQUEST_DURATION = Histogram(
+    "synthetic_request_duration",
+    "Synthetic requests durations",
+    ["method", "url", "result"],
+    buckets=[0.01, 0.1, 0.5, 2, float("inf")],
+)
+
+logger.info("Monitor")
+
+
+@app.route("/")
+@app.route("/up")
+def up():
+    return "I am running"
+
+
+def start_monitor():
+    logger.info("Starting monitor")
+    while True:
+        logger.info("Sending request to reservations service")
+        endpoint = f"{RESERVATIONS_SERVICE_URL}/reservations"
+        result = 'success'
+        start = time.time()
+        try:
+            response = requests.get(endpoint)
+            if response.status_code != 200:
+                result = 'failure'
+        except Exception as e:
+            logger.warn(f"Error: {e}")
+            result = 'failure'
+        finally:
+            end = time.time()
+            HTTP_REQUEST_DURATION.labels(
+                method='GET',
+                url=endpoint,
+                result=result
+            ).observe(end - start)
+
+        time.sleep(INTERVAL_SEC)
+
+    logger.info("Monitor terminated")
+
+
+# Start the monitor in a separate thread in the background
+t = threading.Thread(target=start_monitor)
+t.start()
---
+++ reservations-service/app.py
@@ -0,0 +1,65 @@
+import random
+import time
+
+import structlog
+from flask import Flask, request
+from prometheus_client import Histogram, make_wsgi_app
+from werkzeug import Response
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+
+app = Flask(__name__)
+app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {"/metrics": make_wsgi_app()})
+
+import prometheus_client
+
+prometheus_client.REGISTRY.unregister(prometheus_client.GC_COLLECTOR)
+prometheus_client.REGISTRY.unregister(prometheus_client.PLATFORM_COLLECTOR)
+prometheus_client.REGISTRY.unregister(prometheus_client.PROCESS_COLLECTOR)
+
+
+logger = structlog.get_logger()
+
+
+HTTP_REQUEST_DURATION = Histogram(
+    "http_request_duration",
+    "Requests durations",
+    ["method", "url", "code"],
+    buckets=[0.01, 0.1, 0.5, 2, float("inf")],
+)
+
+
+def observe_http(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        response = func(*args, **kwargs)
+        end = time.time()
+        HTTP_REQUEST_DURATION.labels(
+            method=request.method,
+            code=response.status_code,
+            url=request.url,
+        ).observe(end - start)
+        return response
+
+    return wrapper
+
+
+@app.route("/reservations")
+@observe_http
+def reservations():
+    logger.info("Reservation request")
+    random_duration = (
+        random.choice([1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 3])
+        * random.randint(1, 100)
+        * 0.001
+    )
+    time.sleep(random_duration)
+
+    response_code = random.choice([200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 400, 401, 500])
+    logger.info("Reservation response", code=response_code)
+    return Response(str(response_code), status=response_code)
+
+
+@app.route("/")
+@app.route("/up")
+def up():
+    return "I am running"
